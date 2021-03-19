7da4bc0fda8bb45660986b631783f709d56d03d8
Haskor <shahar.haskor@vayyar.com>
2021-03-19 22:20:54 +0200

    added timer function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from inspect import isfunction
+from threading import Thread
+import time
+import winsound
 
 import webbrowser
 chrome_path = r"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
@@ -10,7 +13,7 @@ def SE_help():
             doc = func.__doc__
             if doc:
                 if doc.startswith('SE:'):
-                    print(f'{function_key}: {doc[2:]}')
+                    print(f'{function_key}: {doc[3:]}')
     for function_key, func in globals().items():
         print_function(function_key, func)
 
@@ -18,6 +21,28 @@ def SE_help():
 def print_something():
     """SE: prints something to screen"""
     print('something')
+
+
+def beep(times=1):
+    frequency = 2500
+    duration = 1000
+    for _ in range(times):
+        winsound.Beep(frequency, duration)
+
+
+def timer_helper(minutes=5, times=1):
+    time.sleep(minutes*60)
+    beep(times)
+
+
+def threaded_func(threaded_function, *args, **kwargs):
+    thread = Thread(target=threaded_function, args=args, kwargs=kwargs)
+    thread.start()
+    thread.join()
+
+
+def timer(minutes=5, times=1):
+    threaded_func(timer_helper, minutes=minutes, times=times)
 
 
 def work(browser='chrome'):
@@ -32,4 +57,5 @@ def open_urls(browser, urls):
     webbrowser.register(browser, None, webbrowser.BackgroundBrowser(chrome_path))
     for url in urls:
         webbrowser.open_new_tab(url)
+
 
